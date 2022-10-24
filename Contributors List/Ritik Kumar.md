23b15b799cd98b785fed6acb7a8247448a25866f
johmn123-wq <iit2019088@iiita.ac.in>
2022-10-24 10:58:56 +0530

    Added segment tree

---
+++ Contributors List/Ritik Kumar.md
@@ -0,0 +1,3 @@
+Ritik Kumar
+https://github.com/johmn123-wq/
+https://www.linkedin.com/in/ritik-kumar-0340931a8/
---
+++ Data Structures and Algorithms/Segment Tree/Range_sum_query.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+#define ll long long 
+
+vector<int>seg_tree;
+vector<int>v;
+
+void build_tree(int st,int en,int node){
+	
+	if(st==en){
+		 seg_tree[node]=v[st];
+		 return;
+	}
+	
+	int mid=(st+en)/2;
+	
+	build_tree(st,mid,2*node+1);
+	build_tree(mid+1,en,2*node+2);
+	
+	seg_tree[node]=seg_tree[2*node+1]+seg_tree[2*node+2];
+
+}
+
+void Update(int value,int index,int st,int en,int node){
+	
+	if(st==en){
+		v[index]=value;
+		seg_tree[node]=v[st];
+		return ;
+	}
+	
+	int mid=(st+en)/2;
+	
+	Update(value,index,st,mid,2*node+1);
+	
+	Update(value,index,mid+1,en,2*node+2);
+	
+	seg_tree[node]=seg_tree[2*node+1]+seg_tree[2*node+2];
+
+}
+
+ll query(ll node,ll st,ll en,ll l,ll r){
+   
+	// l...st....en..r
+	if(l<=st and en<=r)
+		return seg_tree[node];
+	   // r...st...en...l
+	else if(r<st || en<l )
+		return 0;
+	
+	int mid=(st+en)/2;
+	ll left=query(2*node+1,st,mid,l,r);
+	ll right=query(2*node+2,mid+1,en,l,r);
+		// cout<<left<<" "<<right;
+	return left+right;
+}
+int main(){ 
+	
+   int n;cin>>n;
+   seg_tree.resize(4*n);
+   v.resize(n);
+   for(int i=0;i<n;i++){
+          cin>>v[i];
+   }
+   
+   build_tree(0,v.size()-1,0);
+   
+   int q;cin>>q;
+   for(int i=0;i<q;i++){
+  	 int l,r;cin>>l>>r;
+   cout<<"Sum from index "<<l<<" to "<<r<<" is : "<<query(0,0,v.size()-1,l,r)<<endl;
+}
+
+return 0; 
+} 
